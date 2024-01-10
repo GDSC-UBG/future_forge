cecc06f79e3742fbd176de4b4c4c8bbb1b9d9c30
dion <juniordion7@gmail.com>
2024-01-10 15:34:47 +0800

    init project be

---
+++ BE/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const default_router = `api/${process.env.APP_VERSION}`
+//require('./app/routes')(express, app, default_router)
+
+
+module.exports = app
