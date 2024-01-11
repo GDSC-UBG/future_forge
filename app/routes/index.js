7befcfe6d3e1bacbe3f2dc6126a7feb1b7674eb2
dion <juniordion7@gmail.com>
2024-01-12 01:24:19 +0800

    add module daily notes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,5 @@
 module.exports = (express, app, default_router) => {
   require("./auth.route")(express, app, default_router);
   require("./user.route")(express, app, default_router);
+  require('./notes.route')(express, app, default_router);
 };
