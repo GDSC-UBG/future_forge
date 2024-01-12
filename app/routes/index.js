a8c615f518bdcc708687f57bd23211214936ed4a
osi <sayidinaahmadalqososyi@gmail.com>
2024-01-13 00:30:04 +0800

    clean structure

--- app/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = (express, app, default_router) => {
-  require("./auth.route")(express, app, default_router);
-  require("./user.route")(express, app, default_router);
-  require("./calender.route")(express, app, default_router);
-  require("./notes.route")(express, app, default_router)
-  
-};
