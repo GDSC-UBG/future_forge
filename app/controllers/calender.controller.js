afe2d56fcbd825895481c8a3ea16764bfa7a6726
dion <juniordion7@gmail.com>
2024-01-18 18:36:43 +0800

    add crud calender

--- app/controllers/calender.controller.js
+++ app/controllers/calender.controller.js
@@ -6,6 +6,7 @@ const create = (req, res) => {
     ...req.body,
     id_user,
   };
+  console.log(req.body);
   Schadule.create(data)
     .then(() => {
       res
@@ -20,6 +21,95 @@ const create = (req, res) => {
     });
 };
 
+const getAllCalender = async (req, res) => {
+  try {
+    const data = await Schadule.findAll();
+
+    if(data) {
+      return res.status(200).json({
+        msg: 'success retrive all Calender',
+        results: data
+      })
+    }
+
+    return res.status(400).json({
+      msg: 'calender is empty!'
+    })
+
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+}
+
+const getDetailCalender = async (req, res) => {
+  try {
+    const id_schadule = req.params.id_schadule
+
+    const data = await Schadule.findOne({
+      where: {
+        id_schadule
+      }
+    })
+
+    if( data ) {
+      return res.status(200).json({
+        msg: 'succes get Calender',
+        result: data
+      })
+    }
+
+    return res.status(404).json({
+      msg: `Calender is not Found with id: ${id_schadule}`
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    })
+  }
+}
+
+const updateCalender = async (req, res) => {
+  try {
+    const {id_schadule} = req.params
+    await Schadule.update(req.body, {
+      where: {
+        id_schadule
+      }
+    })
+    return res.status(200).json({
+      msg: 'success updated Calender'
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    })
+  }
+}
+
+const deleteCalender = async (req, res) => {
+  try {
+    const {id_schadule} = req.params
+    await Schadule.destroy({
+      where: {
+        id_schadule
+      }
+    })
+
+    return res.status(200).json({
+      msg: 'success deleted calender'
+    })
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message
+    })
+  }
+}
+
 module.exports = {
   create,
+  getAllCalender,
+  getDetailCalender,
+  updateCalender,
+  deleteCalender
 };
--- app/routes/calender.route.js
+++ app/routes/calender.route.js
@@ -1,4 +1,10 @@
-const { create } = require("../controllers/calender.controller");
+const { 
+  create,
+  getAllCalender,
+  getDetailCalender,
+  updateCalender,
+  deleteCalender
+ } = require("../controllers/calender.controller");
 const { verifyToken } = require("../middlewares/auth.jwt");
 const { checkField } = require("../middlewares/schadule");
 require("dotenv").config();
@@ -9,6 +15,10 @@ module.exports = (express, app, default_router = "/api") => {
   // auth
   // console.log(default_router);
   router.post("/calender", [verifyToken, checkField], create);
+  router.get("/calender/:id_schadule", [verifyToken], getAllCalender)
+  router.get("/calende/:id_schadule", [verifyToken], getDetailCalender)
+  router.put("/calender/:id_schadule", [verifyToken], updateCalender)
+  router.delete("/calender/:id_schadule", [verifyToken], deleteCalender)
 
   app.use(default_router, router);
 };
